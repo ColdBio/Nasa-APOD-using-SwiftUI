9ad88d0e0d26437e6fa5416d45542e8cfbba5b5c
LunarColony <yunisfarah4@gmail.com>
2020-09-09 14:21:15 +0100

    Initial Commit

---
+++ Nasa-APOD-in-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nasa-APOD-in-SwiftUI
+//
+//  Created by Yunis Farah on 09/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nasa-APOD-in-SwiftUI/Nasa_APOD_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nasa_APOD_in_SwiftUIApp.swift
+//  Nasa-APOD-in-SwiftUI
+//
+//  Created by Yunis Farah on 09/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct Nasa_APOD_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
