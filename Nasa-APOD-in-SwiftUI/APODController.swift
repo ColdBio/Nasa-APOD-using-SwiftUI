9bc359aa530263b8407925cf4ef22fd257271adb
LunarColony <yunisfarah4@gmail.com>
2020-09-09 20:33:30 +0100

    Xcode was not pushing changes, but now fixed.

---
+++ Nasa-APOD-in-SwiftUI/APODController.swift
@@ -0,0 +1,27 @@
+//
+//  APODController.swift
+//  Nasa-APOD-in-SwiftUI
+//
+//  Created by Yunis Farah on 09/09/2020.
+//
+
+import SwiftUI
+
+class NasaAPI {
+    func loadData(completion: @escaping (DataModel) -> ()) {
+        let url = URL(string: "https://api.nasa.gov/planetary/apod?api_key=iHEnb7Drgn6eHoYQ8OJ4LxRWJl3tCM9EZEZbDmuF")
+
+        URLSession.shared.dataTask(with: url!) { (data, _, _) in
+            do {
+                let recievedData = try JSONDecoder().decode(DataModel.self, from: data!)
+                
+                DispatchQueue.main.async {
+                    completion(recievedData)
+                }
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+}
